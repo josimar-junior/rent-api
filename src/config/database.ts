047e623bf580fd89a16c8a35e21314d081bf3beb
josimar-junior <juniorpaz99@gmail.com>
2020-12-23 23:52:50 -0300

    Configuring sequelize and creating user migrations

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'tddnodeudemy',
+  password: 'tddnodeudemy',
+  database: 'tdd_node_udemy',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
